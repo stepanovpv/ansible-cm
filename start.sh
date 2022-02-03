bd1da1cc02e581c1aab16a2b1455600e5a5b871a
Pavel Stepanov <stepanov_pv@list.ru>
2022-02-03 07:54:59 -0800

    Ansible files changed

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 inventory=/home/pavel/ansible-cm/inventory/hosts.ini
 
-ansible-playbook playbook.yml -i $inventory -kK -u pavel --ask-pass -vv
+ansible-playbook playbook.yml -i $inventory -kK -u pavel -vv
